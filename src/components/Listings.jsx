4bf5d26db4340af30dab0f2753fa7ba751de5595
femsworld <femsworld@gmail.com>
2022-12-29 18:23:23 +0200

    Added Bootstrap

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
 
 export default function Listings() {
   const [countries, SetCountries] = useState([]);
