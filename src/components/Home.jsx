f6a9fc633e3789f776c9789bdbe724cf2767680c
femsworld <femsworld@gmail.com>
2022-12-29 22:57:51 +0200

    Search function updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,20 @@
 import NavBar from "./Navbar";
 import Listings from "./Listings";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Pagination from "./Pagination" 
 
 function Home() {
   const [countries, setCountries] = useState([]);
+  const [searchString, setSearchString] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
 
   const url = "https://restcountries.com/v3.1/all";
+  const urlWithSearchParams = "https://restcountries.com/v3.1/name/";
   const [recordsPerPage] = useState(5);
+
+  const showSearchResult = (searchString) => {
+    setSearchString(searchString.toLowerCase())
+    };
 
   const setCurrent = (curr) => setCurrentPage(curr);
   const indexOfLastRecord = currentPage * recordsPerPage;
@@ -17,18 +23,20 @@ function Home() {
   const nPages = Math.ceil(countries.length / recordsPerPage);
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          return Error("Oh no");
-        }
-        return res.json();
-      })
-      .then((data) => setCountries(data));
-  }, [] );
+    const endpoint = searchString ? urlWithSearchParams + searchString : url
+    fetch(endpoint)
+    .then(res => {
+    if (!res.ok) {
+    return Error("Oh no");
+    }
+    return res.json();
+    })
+    .then(data => setCountries(data));
+    }, [searchString]);
   return (
     <>
-      <NavBar />
+    {console.log(countries)}
+      <NavBar showSearchResult={showSearchResult}/>
       <Listings countries={currentRecords} />
       <Pagination
         datacount={countries.length}
